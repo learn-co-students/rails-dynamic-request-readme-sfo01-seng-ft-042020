0bffe5fc3c75672b76f978325b7c0469898931f1
Guosa_Edebiri <gedebiri@gmail.com>
2020-05-22 01:54:16 -0700

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,3 +3,9 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 end
+
+
+class PostsController < ApplicationController
+  def show
+  end
+end
